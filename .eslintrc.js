f573a77b3beb6a2706561f8d98f1b1fa6963e1ce
Jeremy ALLISON <jeremy.allison@horticulture.red>
2021-12-08 09:32:40 +0100

    chore: enforce code style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,27 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/function-component-definition': 0, // Bug in airbnb rules
+    'max-len': 0,
+    'no-unused-expressions': 0,
+    'no-plusplus': 0,
+    'import/prefer-default-export': 0,
+    'react/jsx-props-no-spreading': 0,
+    'import/no-mutable-exports': 0,
+    'react/forbid-prop-types': 0,
+    'react/no-unescaped-entities': 0,
+    'react/no-array-index-key': 0,
+  },
 };
