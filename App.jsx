c7efc3d9941d1723e1dabe9262985ad67950bd43
Jeremy ALLISON <jeremy.allison@horticulture.red>
2021-12-08 11:01:11 +0100

    feat: add styled-components

--- App.jsx
+++ App.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
+import { SafeAreaView } from 'react-native';
 import store, { persistor } from './app/store/index';
 import { PERSISTENCE } from './app/store/config';
 import { Router } from './app/router';
 
 export const App = () => (
   <Provider store={store}>
-    {PERSISTENCE ? (
-      <PersistGate loading={null} persistor={persistor}>
+    <SafeAreaView>
+      {PERSISTENCE ? (
+        <PersistGate loading={null} persistor={persistor}>
+          <Router />
+        </PersistGate>
+      ) : (
         <Router />
-      </PersistGate>
-    ) : (
-      <Router />
-    )}
+      )}
+    </SafeAreaView>
   </Provider>
 );
---
+++ app/theme.js
@@ -0,0 +1,22 @@
+import { Text } from 'react-native';
+import styled from 'styled-components';
+
+export const ThemeStyles = {
+  mainContainer: {
+    padding: 15,
+  },
+};
+
+export const ThemeColors = {
+  borders: '#3f3f3f',
+};
+
+export const H1 = styled(Text)`
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+
+export const H3 = styled(Text)`
+  font-weight: bold;
+`;
