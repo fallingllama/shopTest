cc57a8770f2724f9b20c26b7baf7e1074fc901a8
Jeremy ALLISON <jeremy.allison@horticulture.red>
2021-12-09 09:17:15 +0100

    fix: use Accept header in GET requests

--- app/hooks/api.js
+++ app/hooks/api.js
@@ -1,17 +1,19 @@
 const API_ENDPOINT = 'https://www.wecasa.fr/api/techtest';
 
 export const useApi = () => {
-  const getRoute = (route) => async () => {
+  const getRoute = route => async () => {
     let jsonRes = false;
 
     try {
-      const res = await fetch(`${API_ENDPOINT}/${route}`, {
+      const res = await fetch(`${API_ENDPOINT}${route}`, {
         method: 'GET',
+        headers: { Accept: 'application/json' },
       });
 
       jsonRes = await res.json();
     } catch (error) {
       // TODO: error mgmt
+      console.log(error);
     }
 
     return jsonRes;
